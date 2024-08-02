1da4a7d7dc1b3ddd24285c6ba702db1f434efef5
Arc prabh <prabharc00@gmail.com>
2024-08-02 21:14:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('button').addEventListener('click', function() {
+    document.querySelector('.initial-message').style.display = 'none';
+    document.querySelector('.photo-view').style.display = 'block';
+});
